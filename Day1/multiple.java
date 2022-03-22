27e7a82f4d479084c38b6835fbff0e55f27321e3
chhaitalmaagade <chaital.m@idigicloudtech.com>
2022-03-22 17:42:08 +0530

    Calculating multiple for 5*3.

--- Day1/multiple.java
+++ Day1/multiple.java
@@ -1,5 +1,8 @@
 public class multiple {
     public static void main(String[] args) {
-        System.out.println("Hello_W!");
+        System.out.println("Hello_World!");
+        int a = 5*3;
+        System.out.println("5*3 is :"+a);
+
     }
 }
---
+++ Day1/seconds.java
@@ -0,0 +1,6 @@
+public class seconds {
+    public static void main(String[] args) {
+        int seconds = 24*60*60;
+        System.out.println("In a day we have :"+seconds);
+    }
+}
